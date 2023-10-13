9817eeab572d729b84eeeaaf7199e2cdd51c05c9
YashaswiniKB <yashashwinikb1999@gmail.com>
2023-10-13 23:39:05 +0530

    Storage Account

--- main.tf
+++ main.tf
@@ -20,3 +20,15 @@ resource "azurerm_resource_group" "Terraform-RG" {
   name     = "Terraform-RG"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "terrformstorage" {
+  name                     = "terraformltistorage"
+  resource_group_name      = azurerm_resource_group.Terraform-RG.name
+  location                 = azurerm_resource_group.Terraform-RG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
