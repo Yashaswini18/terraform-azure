408cbbae320ad6f5cd67035450cd2e3dc41e8e27
YashaswiniKB <yashashwinikb1999@gmail.com>
2023-10-19 16:26:30 +0530

    NSG

--- main.tf
+++ main.tf
@@ -180,3 +180,26 @@ SETTINGS
 
 }
 
+//NSG
+resource "azurerm_network_security_group" "nsg-name" {
+  name                = "terrafom-NSG"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.Terraform-RG.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-association" {
+  subnet_id                 = azurerm_subnet.subnet-name.id
+  network_security_group_id = azurerm_network_security_group.nsg-name.id
+}
