431f2f3764bd0e0d00c86ef872b2b271cdda8e43
YashaswiniKB <yashashwinikb1999@gmail.com>
2023-10-21 01:52:45 +0530

    App service

--- main.tf
+++ main.tf
@@ -281,3 +281,42 @@ resource "local_file" "local-file-name" {
   content = tls_private_key.ssh-private-key-name.private_key_pem
 }
 
+resource "azurerm_app_service_plan" "plan-name" {
+  name                = "terraform-plan-name"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.Terraform-RG.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app-service-name" {
+  name                = "terraformapp00009274823"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.Terraform-RG.name
+  app_service_plan_id = azurerm_app_service_plan.plan-name.id
+
+  site_config {
+    dotnet_framework_version = "v6.0"
+   # scm_type                 = "LocalGit"
+  }
+
+  # app_settings = {
+  #   "SOME_KEY" = "some-value"
+  # }
+
+  # connection_string {
+  #   name  = "Database"
+  #   type  = "SQLServer"
+  #   value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  # }
+
+  source_control {
+    repo_url = "https://github.com/alashro/webapp"
+    branch = "master"
+    manual_integration = true
+    use_mercurial = false
+  }
+}
