178bc19a407e41fa5beb24e1df291ddfa0849f22
Yashaswini <55853218+Yashaswini18@users.noreply.github.com>
2024-03-02 23:45:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,6 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "92813963-bb62-468f-b6b4-bda395b75676"
-  client_id = "f5efa60c-6afa-4094-9378-f61d8683941a"
-  client_secret = "pIb8Q~byx_3iBd4OmISpydLZqolqFjfswYlTGbUG"
-  tenant_id = "02aa9fc1-18bc-4798-a020-e01c854dd434"
   features { }
 }
 
